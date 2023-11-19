ae3f92f0cea6c1fa95feeddc16f5c8f834b402b1
nemoguigrat <sharos185@gmail.com>
2023-11-19 12:02:35 +0500

    document short spec

--- src/main/java/com/example/mayo/journey/repository/jdbc/DocumentShortRepository.java
+++ src/main/java/com/example/mayo/journey/repository/jdbc/DocumentShortRepository.java
@@ -1,23 +1,10 @@
 package com.example.mayo.journey.repository.jdbc;
 
 import com.example.mayo.journey.domain.jdbc.DocumentShort;
-import com.example.mayo.journey.support.DocumentStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentShortRepository extends JpaRepository<DocumentShort, Long> {
-
-    @Query(value = "select distinct ds from DocumentShort ds where ds.documentIndex.status = :status and " +
-            "(:themeId is null or (:themeId is not null and ds.documentIndex.theme.id = :themeId)) ",
-            countQuery = "select count(ds.id) from DocumentShort ds where ds.documentIndex.status = :status and " +
-                    "(:themeId is null or (:themeId is not null and ds.documentIndex.theme.id = :themeId)) ")
-    Page<DocumentShort> findByFilter(DocumentStatus status, Long themeId, Pageable pageable);
-
-    @Query(value = "select distinct ds from DocumentShort ds where ds.documentIndex.user.id = :userId ",
-            countQuery = "select count(ds.id) from DocumentShort ds where ds.documentIndex.user.id = :userId")
-    Page<DocumentShort> findByUser(Long userId, Pageable pageable);
+public interface DocumentShortRepository extends JpaRepository<DocumentShort, Long>, JpaSpecificationExecutor<DocumentShort> {
 }
